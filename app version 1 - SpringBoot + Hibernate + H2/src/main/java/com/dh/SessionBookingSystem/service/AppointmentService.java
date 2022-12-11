86209315b59eaae05cfbba4a3a7c3eb6668adde8
florencialecha <flecharegueira@gmail.com>
2022-12-11 19:12:53 -0300

    Add findById method on  appointment

--- app version 1 - SpringBoot + Hibernate + H2/src/main/java/com/dh/SessionBookingSystem/service/AppointmentService.java
+++ app version 1 - SpringBoot + Hibernate + H2/src/main/java/com/dh/SessionBookingSystem/service/AppointmentService.java
@@ -4,23 +4,45 @@ import com.dh.SessionBookingSystem.dto.AppointmentDTO;
 import com.dh.SessionBookingSystem.entity.Appointment;
 import com.dh.SessionBookingSystem.entity.Dentist;
 import com.dh.SessionBookingSystem.entity.Patient;
+import com.dh.SessionBookingSystem.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class AppointmentService {
+
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
     public Optional<AppointmentDTO> findById(Long id) {
+        Optional<Appointment> searchAppointment = appointmentRepository.findById(id);
+        if (searchAppointment.isEmpty()) {
+            return Optional.empty();
+        }
+        else {
+            return Optional.of(appointmentToAppointmentDTO(searchAppointment.get()));
+        }
     }
 
     private AppointmentDTO appointmentToAppointmentDTO(Appointment appointment) {
+
         AppointmentDTO response = new AppointmentDTO();
+
         response.setId(appointment.getId());
         response.setAppointmentDate(appointment.getAppointmentDate());
         response.setDentistId(appointment.getDentist().getId());
         response.setPatientId(appointment.getPatient().getId());
+
         return response;
+
     }
+
     private Appointment appointmentDTOToAppointment(AppointmentDTO appointmentDTO) {
 
         Appointment appointment = new Appointment();
@@ -29,12 +51,11 @@ public class AppointmentService {
 
         patient.setId(appointmentDTO.getId());
         dentist.setId(appointmentDTO.getDentistId());
-
         appointment.setId(appointmentDTO.getId());
         appointment.setAppointmentDate(appointmentDTO.getAppointmentDate());
 
         return appointment;
+
     }
 
-    private AppointmentDTO appointment
 }
